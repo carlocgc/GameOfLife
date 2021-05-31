24104f0118360cbe596d94988431676378cc1492
Carlo Mongiello <carlomongiello@improbable.io>
2021-05-31 09:23:24 +0100

    renaming

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,102 +1,56 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+
 class application : public olc::PixelGameEngine
 {
 private:
-	std::wstring m_entities;
-	int m_width;
-	int m_height;
-	int high_threshold;
-	int low_threshold;
-	int random_threshold;
-	float interval;
-	float elapsed;
+	std::wstring m_cells;
+	int m_cell_width;
+	int m_cell_height;
+	int m_grid_width;
+	int m_grid_height;
+	int m_become_alive;
+	int m_become_dead;
+	int m_seed_threshold;
+	float m_update_interval;
+	float m_time_since_last_update;
 
 public:
+	static const int screen_width = 1920; // modify
+	static const int screen_height = 1080; // modify
+	static const int pixel_width = 1;
+	static const int pixel_height = 1;
+
 	application()
+		: m_grid_width(0)
+		, m_grid_height(0)
+		, m_become_alive(3)
+		, m_become_dead(2)
+		, m_seed_threshold(50)
+		, m_update_interval(0.2f)
+		, m_time_since_last_update(0.f)
 	{
 		sAppName = "Game of Life";
-		m_width = 64;
-		m_height = 64;
-		high_threshold = 3;
-		low_threshold = 2;
-		random_threshold = 50;
-		interval = 0.2f;
-		elapsed = 0.f;		
+		m_cell_width = 4; // modify
+		m_cell_height = 4; // modify
 	}
 
 	bool OnUserCreate() override
 	{
-		m_entities += L"..#.............................................................";
-		m_entities += L"#.#.............................................................";
-		m_entities += L".##.............................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
+		const auto screen_size = GetWindowSize();
 
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
+		m_grid_width = screen_size.x / m_cell_width;
+		m_grid_height = screen_size.y / m_cell_height;
 
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
+		const auto cell_count = m_grid_width * m_grid_height;
 
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
-		m_entities += L"................................................................";
+		for (int i = 0; i < cell_count; ++i)
+		{
+			m_cells += L'.';
+		}
 
-		seed_grid(random_threshold);
+		seed_grid(m_seed_threshold);
 
 		return true;
 	}
@@ -106,103 +60,100 @@ public:
 		// check for mouse click
 
 		if (GetMouse(0).bReleased)
-		{			
+		{
 			clear_grid();
-			random_threshold = std::min(random_threshold + 10, 100);
-			seed_grid(random_threshold);
+			m_seed_threshold = std::min(m_seed_threshold + 10, 100);
+			seed_grid(m_seed_threshold);
 			return true;
 		}
-		
+
 		if (GetMouse(1).bReleased)
 		{
-			clear_grid();	
-			random_threshold = std::max(random_threshold - 10, 0);			
-			seed_grid(random_threshold);
+			clear_grid();
+			m_seed_threshold = std::max(m_seed_threshold - 10, 0);
+			seed_grid(m_seed_threshold);
 			return true;
 		}
 
 		// delay update
-		
-		elapsed += fElapsedTime;
 
-		if (elapsed < interval)
+		m_time_since_last_update += fElapsedTime;
+
+		if (m_time_since_last_update < m_update_interval)
 		{
 			return true;
 		}
 
-		elapsed = 0;
+		m_time_since_last_update = 0;
 
-		// Draw entities
+		// Draw cells
 
-		const int fill_width = 16;
-		const int fill_height = 16;
-
-		for (auto y = 0; y < m_height; ++y)
+		for (auto y = 0; y < m_grid_height; ++y)
 		{
-			for (auto x = 0; x < m_width; ++x)
+			for (auto x = 0; x < m_grid_width; ++x)
 			{
-				const auto entity = get_entity(x, y);
-				switch (entity)
+				const auto cell = get_cell(x, y);
+				switch (cell)
 				{
 				case L'.': // dead
-					FillRect(fill_width * x, fill_height * y, fill_width, fill_height, olc::DARK_GREY);
+					FillRect(m_cell_width * x, m_cell_height * y, m_cell_width, m_cell_height, olc::DARK_GREY);
 					break;
 				case L'#': // alive
-					FillRect(fill_width * x, fill_height * y, fill_width, fill_height, olc::GREY);
+					FillRect(m_cell_width * x, m_cell_height * y, m_cell_width, m_cell_height, olc::GREY);
 					break;
 				default:
-					FillRect(fill_width * x, fill_height * y, fill_width, fill_height, olc::BLACK);
+					FillRect(m_cell_width * x, m_cell_height * y, m_cell_width, m_cell_height, olc::BLACK);
 					break;
 				}
 			}
 		}
 
-		// Update entity states
+		// Update cell states
 
-		for (auto y = 0; y < m_height; ++y)
+		for (auto y = 0; y < m_grid_height; ++y)
 		{
-			for (auto x = 0; x < m_width; ++x)
+			for (auto x = 0; x < m_grid_width; ++x)
 			{
 				const auto alive_neighbors = get_alive_neighbors(x, y);
-				const auto entity = get_entity(x, y);
+				const auto cell = get_cell(x, y);
 
-				if (entity == L'#' && (alive_neighbors == low_threshold || alive_neighbors == high_threshold)) // Any live cell with two or three live neighbors survives.
+				if (cell == L'#' && (alive_neighbors == m_become_dead || alive_neighbors == m_become_alive)) // Any live cell with two or three live neighbors survives.
 				{
 					continue;
 				}
-				
-				if (entity == L'.' && alive_neighbors == high_threshold) // Any dead cell with three live neighbors becomes a live cell.
+
+				if (cell == L'.' && alive_neighbors == m_become_alive) // Any dead cell with three live neighbors becomes a live cell.
 				{
-					set_entity(x, y, L'0'); // to become alive
+					set_cell(x, y, L'0'); // to become alive
 				}
 				else // All other live cells die in the next generation.Similarly, all other dead cells stay dead.
 				{
-					set_entity(x, y, entity == L'#' ? L'X' : L'.'); // to be killed
+					set_cell(x, y, cell == L'#' ? L'X' : L'.'); // to be killed
 				}
 			}
 		}
 
-		// Finalize entity states
+		// Finalize cell states
 
-		for (auto y = 0; y < m_height; ++y)
+		for (auto y = 0; y < m_grid_height; ++y)
 		{
-			for (auto x = 0; x < m_width; ++x)
+			for (auto x = 0; x < m_grid_width; ++x)
 			{
-				const auto entity = get_entity(x, y);
+				const auto cell = get_cell(x, y);
 
-				switch (entity)
+				switch (cell)
 				{
 				case L'X': // to be killed
-					set_entity(x, y, L'.');
+					set_cell(x, y, L'.');
 					break;
 				case L'0': // to become alive
-					set_entity(x, y, L'#');
+					set_cell(x, y, L'#');
 					break;
-				default:					
+				default:
 					break;
 				}
 			}
-		}		
+		}
 
 		return true;
 	}
@@ -212,30 +163,30 @@ public:
 		return true;
 	}
 
-	wchar_t get_entity(int x, int y)
-	{	
-		if (x >= 0 && x < m_width && y >= 0 && y < m_height)
+	wchar_t get_cell(int x, int y)
+	{
+		if (x >= 0 && x < m_grid_width && y >= 0 && y < m_grid_height)
 		{
-			return m_entities[y * m_width + x]; // access 1d array like a 2d array
+			return m_cells[y * m_grid_width + x]; // access 1d array like a 2d array
 		}
 		return L' '; // sudo null
 	}
 
-	void set_entity(int x, int y, wchar_t c)
-	{		
-		if (x >= 0 && x < m_width && y >= 0 && y < m_height)
+	void set_cell(int x, int y, wchar_t c)
+	{
+		if (x >= 0 && x < m_grid_width && y >= 0 && y < m_grid_height)
 		{
-			m_entities[y * m_width + x] = c;
+			m_cells[y * m_grid_width + x] = c;
 		}
 	}
 
 	int get_alive_neighbors(int x, int y)
 	{
 		const int x_start = x > 0 ? x - 1 : x;
-		const int x_end = x < m_width ? x + 1 : x;		
+		const int x_end = x < m_grid_width ? x + 1 : x;
 
 		const int y_start = y > 0 ? y - 1 : y;
-		const int y_end = y < m_height ? y + 1 : y;
+		const int y_end = y < m_grid_height ? y + 1 : y;
 
 		int alive_count = 0;
 
@@ -248,9 +199,9 @@ public:
 					continue; // ignore self
 				}
 
-				const auto entity = get_entity(x_pos, y_pos);
+				const auto cell = get_cell(x_pos, y_pos);
 
-				if (entity == L'#' || entity == L'X') // if alive or flagged for death
+				if (cell == L'#' || cell == L'X') // if alive or flagged for death
 				{
 					alive_count++;
 				}
@@ -262,17 +213,17 @@ public:
 
 	void seed_grid(int threshold)
 	{
-		for (auto y = 0; y < m_height; ++y)
+		for (auto y = 0; y < m_grid_height; ++y)
 		{
-			for (auto x = 0; x < m_width; ++x)
+			for (auto x = 0; x < m_grid_width; ++x)
 			{
 				if (rand() % 100 > threshold)
 				{
-					set_entity(x, y, L'#');					
+					set_cell(x, y, L'#');
 				}
 				else
 				{
-					set_entity(x, y, L'.');					
+					set_cell(x, y, L'.');
 				}
 			}
 		}
@@ -280,11 +231,11 @@ public:
 
 	void clear_grid()
 	{
-		for (auto y = 0; y < m_height; ++y)
+		for (auto y = 0; y < m_grid_height; ++y)
 		{
-			for (auto x = 0; x < m_width; ++x)
+			for (auto x = 0; x < m_grid_width; ++x)
 			{
-				set_entity(x, y, L'.');
+				set_cell(x, y, L'.');
 			}
 		}
 	}
@@ -293,7 +244,7 @@ public:
 int main()
 {
 	application app;
-	if (app.Construct(1024, 1024, 1, 1))
+	if (app.Construct(application::screen_width, application::screen_height, application::pixel_width, application::pixel_height))
 	{
 		app.Start();
 	}
